b3e1cdaf5e1a62449abe2e903c242418fa46f528
WeatheredScientist <patarroyo@gmail.com>
2026-05-22 19:22:00 -0400

    Update dewpoint_service.py

--- dewpoint_service.py
+++ dewpoint_service.py
@@ -62,43 +62,4 @@ class DewpointCacher(StdService):
         # Calculate dewpoint and heatindex if we have both values
         if self.last_temp is not None and self.last_humidity is not None:
             packet['dewpoint'] = weewx.wxformulas.dewpointF(self.last_temp, self.last_humidity)
-            packet['heatindex'] = weewx.wxformulas.heatindexF(self.last_temp, self.last_humidity)import weewx
-from weewx.engine import StdService
-import weewx.wxformulas
-
-class DewpointCacher(StdService):
-    def __init__(self, engine, config_dict):
-        super().__init__(engine, config_dict)
-        self.last_temp = None
-        self.last_humidity = None
-        self.last_radiation = None
-        self.last_uv = None
-        self.bind(weewx.NEW_LOOP_PACKET, self.new_loop_packet)
-
-    def new_loop_packet(self, event):
-        packet = event.packet
-
-        # Update cache with any new values
-        if packet.get('outTemp') is not None:
-            self.last_temp = packet['outTemp']
-        if packet.get('outHumidity') is not None:
-            self.last_humidity = packet['outHumidity']
-        if packet.get('radiation') is not None:
-            self.last_radiation = packet['radiation']
-        if packet.get('UV') is not None:
-            self.last_uv = packet['UV']
-
-        # Inject cached values
-        if packet.get('outTemp') is None and self.last_temp is not None:
-            packet['outTemp'] = self.last_temp
-        if packet.get('outHumidity') is None and self.last_humidity is not None:
-            packet['outHumidity'] = self.last_humidity
-        if packet.get('radiation') is None and self.last_radiation is not None:
-            packet['radiation'] = self.last_radiation
-        if packet.get('UV') is None and self.last_uv is not None:
-            packet['UV'] = self.last_uv
-
-        # Calculate dewpoint and heatindex if we have both values
-        if self.last_temp is not None and self.last_humidity is not None:
-            packet['dewpoint'] = weewx.wxformulas.dewpointF(self.last_temp, self.last_humidity)
             packet['heatindex'] = weewx.wxformulas.heatindexF(self.last_temp, self.last_humidity)
